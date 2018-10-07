5451b8f002346b4c8fd2e6034a109700dcec3c10
MarycBrown02 <marycbrown01@sbcgloba.net>
2018-10-06 23:02:50 -0500

    cange css javascript and html

---
+++ assets/javasceipt/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDTGfuEkMKqZB07DFe2kSFKugnOTdM_KCA",
+    authDomain: "mcb-trainscedule.firebaseapp.com",
+    databaseURL: "https://mcb-trainscedule.firebaseio.com",
+    projectId: "mcb-trainscedule",
+    storageBucket: "mcb-trainscedule.appspot.com",
+    messagingSenderId: "164961917551"
+  };
+  firebase.initializeApp(config);
